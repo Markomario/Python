013b9295c060d324f63d2aae39b1b3f4a3c0f6fa
Markus Lindström <markuslindst@gmail.com>
2016-11-16 15:39:53 +0100

    'ny' is a broken version. Loggin is the working version.

--- LogginLyckasFunk.py
+++ LogginLyckasFunk.py
@@ -1,60 +1,54 @@
-import time
 import sys
+import time
+def exit():
+    sys.exit()
 
 progge = "y"
-line = "y"
-while progge[0] == ("y") not in ("n"):
+line = "j"
+
+while progge [0] == ("y") not in ("n"):
     user = "Markus"
 
-    login = input("Hej. Vad är min mästares namn?: ")
+    loggin = input("Vad heter annändaren på denna datorn?: ")
 
-    if login == user:
-        print("Laddar...")
-        time.sleep(3)
-        while line[0] == ("y") not in ("n"):
-            kalk = input("Välkommen " + user + "!\nHur vill du räkna?\n+\n-\n*\n/\n: ")
+    if loggin == "Markus":
+        line = "j"
+        while line[0] == ("j") not in ("n"):
+            kalk = input("Hej igen " + user + "!\nHur vill du räkna?\n+\n-\n*\n/\n: ")
             number1 = int(input("Välj det första nummret som du vill räkna med: "))
             number2 = int(input("Välj det andra nummret som du vill räkna med: "))
-
             if kalk == "+":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", summan blir:", number1 + number2)
-                line = input("Vill du starta om programmet? (y/n): ")
+                line = input("Vill du starta om programmet? (j/n): ")
             elif kalk == "-":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", differensen blir:", number1 - number2)
-                line = input("Vill du starta om programmet? (y/n): ")
+                line = input("Vill du starta om programmet? (j/n): ")
             elif kalk == "*":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", produkten blir:", number1 * number2)
-                line = input("Vill du starta om programmet? (y/n): ")
+                line = input("Vill du starta om programmet? (j/n): ")
             elif kalk == "/":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", kvoten blir:", number1 / number2)
-                line = input("Vill du starta om programmet? (y/n): ")
+                line = input("Vill du starta om programmet? (j/n): ")
             else:
+                print("Laddar...")
                 time.sleep(2)
-                line = input("Något verkar ha gått snett nu. Vill du försöka igen? (y/n): ")
-
-
+                line = input("Något verkar ha gått snett nu. Vill du försöka igen? (j/n): ")
+        if line == ("n"):
+            progge = "n"
     else:
-        print("Laddar...")
-        time.sleep(3)
-        print("Nähädu, det här var fel, din dumstrut!")
-        progge = input("Vill du försöka igen? (y/n): ")
+        print("nu gick det fel")
+        progge = input("vill du avsluta?")
 
-while line == "n":
-    progge = "n"
-
-def exit():
-    sys.exit()
-
-while progge[0] == ("n"):
+while progge == ("n"):
     print("Ok, ha en bra dag!")
     time.sleep(2)
-    print("Stänger av nu.")
+    print("Stänger av nu...")
     exit()
