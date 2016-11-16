2882a75a77bd26fb5082f1df242db431919e0d97
Markus Lindström <markuslindst@gmail.com>
2016-11-16 16:02:41 +0100

    Den nya räknaren som faktiskt fungerar!

--- LogginLyckasFunk.py
+++ LogginLyckasFunk.py
@@ -9,46 +9,49 @@ line = "j"
 while progge [0] == ("y") not in ("n"):
     user = "Markus"
 
-    loggin = input("Vad heter annändaren på denna datorn?: ")
+    loggin = input("Vad heter min mästare?: ")
 
     if loggin == "Markus":
         line = "j"
         while line[0] == ("j") not in ("n"):
-            kalk = input("Hej igen " + user + "!\nHur vill du räkna?\n+\n-\n*\n/\n: ")
+            print("Laddar...")
+            time.sleep(3)
+            kalk = input("Mästare " + user + "!\nHur vill du räkna?\nAddition,\nSubbtraktion,\nMultiplikation,\neller Divition?\n(Skriv tecknet, inte ordet)\n: ")
             number1 = int(input("Välj det första nummret som du vill räkna med: "))
             number2 = int(input("Välj det andra nummret som du vill räkna med: "))
             if kalk == "+":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", summan blir:", number1 + number2)
-                line = input("Vill du starta om programmet? (j/n): ")
+                line = input("Vill du räkna igen? (j/n): ")
             elif kalk == "-":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", differensen blir:", number1 - number2)
-                line = input("Vill du starta om programmet? (j/n): ")
+                line = input("Vill du räkna igen? (j/n): ")
             elif kalk == "*":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", produkten blir:", number1 * number2)
-                line = input("Vill du starta om programmet? (j/n): ")
+                line = input("Vill du räkna igen? (j/n): ")
             elif kalk == "/":
                 print("Laddar....")
                 time.sleep(3)
                 print(user + ", kvoten blir:", number1 / number2)
-                line = input("Vill du starta om programmet? (j/n): ")
+                line = input("Vill du räkna igen? (j/n): ")
             else:
                 print("Laddar...")
                 time.sleep(2)
-                line = input("Något verkar ha gått snett nu. Vill du försöka igen? (j/n): ")
+                line = input("Något verkar ha gått snett. Vill du försöka igen? (j/n): ")
         if line == ("n"):
             progge = "n"
     else:
-        print("nu gick det fel")
-        progge = input("vill du avsluta?")
+        print("Du är inte min mästare!")
+        progge = input("Vill du försöka att logga in igen?")
 
 while progge == ("n"):
     print("Ok, ha en bra dag!")
-    time.sleep(2)
+    time.sleep(1)
     print("Stänger av nu...")
+    time.sleep(2)
     exit()
